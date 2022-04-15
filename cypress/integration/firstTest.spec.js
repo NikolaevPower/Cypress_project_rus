4019479bd4680cfb8089b10ca9ee1490aa77d9c3
Oleg Nikolaev <zaycevr@bk.ru>
2022-04-15 19:59:31 +0200

    Delete comment

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,40 +1,40 @@
 ///<reference types="Cypress"/>
 
-// describe('Mobile phone replenishment',() => {
-//   context ('Replenishment less than the allowed amount',() => {
-//      it ('Show error: Minimum amount of the replenishment 1 UAH'),() => {
+describe('Mobile phone replenishment',() => {
+  context ('Replenishment less than the allowed amount',() => {
+     it ('Show error: Minimum amount of the replenishment 1 UAH'),() => {
 
-//      }
-//   })
-// })
+     }
+  })
+})
 
 
-// it ('By ID',() => {
-//    cy.visit('https://www.facebook.com/')
-//    cy.contains('Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie').click()
-//    cy.get('.fcb > a').click()
-//    cy.contains('Разрешить основные и необязательные cookie').click()
-//    cy.get('#email')
-// })
+it ('By ID',() => {
+   cy.visit('https://www.facebook.com/')
+   cy.contains('Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie').click()
+   cy.get('.fcb > a').click()
+   cy.contains('Разрешить основные и необязательные cookie').click()
+   cy.get('#email')
+})
 
-// it ('By Class',() => {
-//    cy.visit('https://docs.cypress.io/api/commands/get')
-//    cy.get('[class="DocSearch DocSearch-Button"]')
-// })
+it ('By Class',() => {
+   cy.visit('https://docs.cypress.io/api/commands/get')
+   cy.get('[class="DocSearch DocSearch-Button"]')
+})
 
-// it ('By Tag',() => {
-//    cy.visit('https://docs.cypress.io/api/commands/get')
-//    cy.get('nav')
-// })
+it ('By Tag',() => {
+   cy.visit('https://docs.cypress.io/api/commands/get')
+   cy.get('nav')
+})
 
-// it.only('By Different Tag',() => {
-//    cy.visit('https://www.facebook.com/')
-//    cy.contains('Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie').click()
-//    cy.get('.fcb > a').click()
-//    cy.contains('Разрешить основные и необязательные cookie').click()
-//    cy.get('[data-testid="open-registration-form-button"][role="button"]')
-// })
-//Comment_Snoopy
+it.only('By Different Tag',() => {
+   cy.visit('https://www.facebook.com/')
+   cy.contains('Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie').click()
+   cy.get('.fcb > a').click()
+   cy.contains('Разрешить основные и необязательные cookie').click()
+   cy.get('[data-testid="open-registration-form-button"][role="button"]')
+})
+
 
 it('Using Get with Find and Eq',() => {
    cy.visit('https://next.privat24.ua/deposit/open')
