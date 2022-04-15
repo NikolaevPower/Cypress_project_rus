6e118d13cb1dbe38318460c460406c9859515540
Oleg Nikolaev <zaycevr@bk.ru>
2022-04-15 21:43:40 +0200

    new branch findElWithContains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,1 +1,21 @@
 ///<reference types="Cypress"/>
+
+it('',() => {
+   cy.visit('https://next.privat24.ua/?lang=en')
+   cy.contains('Sign in')
+})
+
+it('',() => {
+   cy.visit('https://next.privat24.ua/?lang=en')
+   cy.contains('div','Sign in')
+})
+
+it('',() => {
+   cy.visit('https://next.privat24.ua/?lang=en')
+   cy.contains('SIGN IN', {matchCase:false});
+})
+
+it.only('',() => {
+   cy.visit('https://next.privat24.ua/?lang=en')
+   cy.get('footer').contains('© 2022 PrivatBank NBU license No. 22 dated March 19, 1992');
+})
